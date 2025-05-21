f0cf28be76e34331f110462686e577a13520b856
agush44 <magustinabenitez@gmail.com>
2025-05-20 22:12:44 -0300

    add: input sanitization

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -5,15 +5,18 @@ import {
   IsOptional,
   IsBoolean,
 } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class CreateUserDto {
   @IsString()
   @MinLength(4)
   @MaxLength(100)
+  @Transform(({ value }: { value: string }) => value.trim().toLowerCase())
   username!: string;
 
   @IsString()
   @MinLength(6)
+  @Transform(({ value }: { value: string }) => value.trim())
   password!: string;
 
   @IsOptional()
